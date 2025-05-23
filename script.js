8dd5a5051f2de6102c7b695bfd8d15c72ca0598e
Ítalo Conceição <56076361+italoconceicao@users.noreply.github.com>
2025-05-23 07:23:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let etiquetaBipada
 
 document.getElementById('fileInput').addEventListener('change', function (e) {
+
+    const containerBusca = document.getElementById('containerBusca')
+    document.getElementById('secaoBusca').style.display = "none";
+    containerBusca.style.display = 'block';
+
     const file = e.target.files[0];
     if (!file) return;
 
@@ -10,44 +16,62 @@ document.getElementById('fileInput').addEventListener('change', function (e) {
         const data = new Uint8Array(e.target.result);
         const workbook = XLSX.read(data, { type: 'array' });
 
-        // Converter a primeira planilha para JSON
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
         const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-        // Exibir o resultado
-        const resultDiv = document.getElementById('result');
-        const jsonOutput = document.getElementById('jsonOutput');
-
-        //Filtro                   <==
-        // const filtroEtiquetaBipada = jsonData.filter((produto) => produto["Nro. Etiqueta"] == etiquetaBipada);
-        const filtroEtiquetaBipada = jsonData.filter((produto) => produto["Bairro Pessoa Visita"] == "CANUDOS");
-        if (filtroEtiquetaBipada.length > 0) {
-            console.log("deu")
-        } else { console.log("Ué?!") }
-
         const resultado = []
+        const remessasEncontradas = []
 
-        for (remessa of filtroEtiquetaBipada) {
-            resultado.push(
-                {
-                    "Etiqueta": remessa["Nro. Etiqueta"],
-                    "Status": remessa["Status"],
-                    "Nome do Cliente": remessa["Nome Pessoa Visita"]
+        // console.log(remessasEncontradas)
+
+        document.getElementById('btnBusca').addEventListener('click', function (e) {
+            etiquetaBipada = document.getElementById('buscaEtiqueta').value;
+
+
+            const filtroEtiquetaBipada = jsonData.filter((produto) => produto["Nro. Etiqueta"] == etiquetaBipada);
+            const filtroRemessasEncontradas = remessasEncontradas.filter((produto) => produto["Nro. Etiqueta"] == etiquetaBipada);
+
+            for (remessa of filtroEtiquetaBipada) {
+
+                const jaEsta = filtroRemessasEncontradas.filter((remessa) => remessa["Nro. Etiqueta"] == etiquetaBipada)
+
+                if (jaEsta.length > 0) {
+                    alert("Remessa já adicioda.")
+                } else {
+                    remessasEncontradas.push(remessa);
+
+                    resultado.push(`✅ ${remessa["Rota"]} - ${remessa["Nome Pessoa Visita"]} -  ${remessa["Logradouro Pessoa Visita"]}, ${remessa["Numero Endereço Pessoa Visita"]}`)
                 }
-            )
-        }
 
-        console.log(resultado)
-        jsonOutput.textContent = JSON.stringify(resultado, null, 2);
-        resultDiv.style.display = 'block';
+            }
 
-        // Opcional: fazer download do JSON
-        createDownloadLink(jsonData, file.name);
+            const resultDiv = document.getElementById('result');
+            const jsonOutput = document.getElementById('jsonOutput');
+
+            // jsonOutput.textContent = JSON.stringify(resultado, null, 2);
+
+            if (resultado.length > 0) {
+                jsonOutput.textContent = resultado.join('\n');
+            } else {
+                alert('Nenhum vencimento encontrado para esta etiqueta.');
+            }
+            resultDiv.style.display = 'block';
+
+
+            // Opcional: fazer download do JSON
+            // createDownloadLink(jsonData, file.name);
+        });
+
+
+
+
     };
 
     reader.readAsArrayBuffer(file);
 });
+
+
 
 // Função opcional para criar link de download
 function createDownloadLink(data, originalFilename) {
